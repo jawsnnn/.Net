efe8413a6193ddb3fc5b349f8e68fea792ed57e1
jawsnnn <arpanmalviya@gmail.com>
2017-09-21 07:45:05 -0400

    Exception handlling and refactoring of code

--- GradeBook/Class1.cs
+++ GradeBook/Class1.cs
@@ -4,35 +4,91 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GradeBook;
+using System.IO;
 
 namespace GradeBook
 {
     class Program
     {
-    static void Main(string[] args)
+        static void Main(string[] args)
         {
-            GradeBook gb = new GradeBook("My gradebook");
 
-            gb.Name = null;
-
-            Console.WriteLine(gb.Name);
-            gb.AddGrade(10);
-            gb.AddGrade(10.9F);
-
-            GradeBook book2 = gb;
-            book2.AddGrade(21.0F);
-
-            gb.WriteGrades(Console.Out);
-
-            GradeStatistics stats = gb.ComputeStats();
-            WriteResults("Average", stats.AvgGrade);
-            WriteResults("Minimum grade", stats.MinGrade);
+            GradeBook gb = InitializeGradeBook();
+            AddGrades(gb);
+            WriteGrades(gb);
+            DisplayStats(gb);
             //Console.WriteLine("Min: " + stats.MinGrade + "|Max: " + stats.MaxGrade + "|Avg: " + stats.AvgGrade);
         }
 
-    static void WriteResults(string desc, float result)
+        private static void DisplayStats(GradeBook gb)
+        {
+            GradeStatistics stats = gb.ComputeStats();
+            WriteResults("Average", stats.AvgGrade);
+            WriteResults("Minimum grade", stats.MinGrade);
+            WriteResults("Maximum grade", stats.MaxGrade);
+        }
+
+        private static void WriteGrades(GradeBook gb)
+        {
+            using (StreamWriter f = File.CreateText("Results.txt"))
+            {
+                gb.WriteGrades(f);
+            }
+        }
+
+        private static void AddGrades(GradeBook gb)
+        {
+            gb.AddGrade(10);
+            gb.AddGrade(10.9F);
+            gb.AddGrade(21.0F);
+        }
+
+        static int validateGradeBookName(GradeBook book, string name)
+        {
+            try
+            {
+                book.Name = name;
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try again");
+                return 1;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return 0;
+        }
+
+        private static GradeBook InitializeGradeBook()
+        {
+            GradeBook gb = new GradeBook("My gradebook");
+            gb.onNameChanged += ChangingName;
+            gb.WelcomeNewVal += ChangedName;
+
+            string book_name;
+            do
+            {
+                Console.WriteLine("Enter a gradebook name");
+                book_name = Console.ReadLine();
+            } while (validateGradeBookName(gb, book_name) == 1);
+            Console.WriteLine(gb.Name);
+            return gb;
+        }
+
+        static void WriteResults(string desc, float result)
         {
             Console.WriteLine(desc + ": " + result);
+        }
+        static void ChangingName(object sender, NameChangedEventArgs args)
+        {
+            Console.WriteLine($"Try changing name from {args.ExistingName} to {args.NewName}");
+        }
+        static void ChangedName(object sender, NameChangedEventArgs args)
+        {
+            Console.WriteLine($"Changed name to {args.NewName}");
         }
     }
 }
